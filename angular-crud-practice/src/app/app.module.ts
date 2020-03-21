fbe20ec9f62e83762fe7b0c8f1658515c12b9176
shyamuu1 <shyamuu.vasanjee@gmail.com>
2020-03-21 00:00:43 -0400

    new components and bootstrap4 added

--- angular-crud-practice/src/app/app.module.ts
+++ angular-crud-practice/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SignupUserComponent } from './signup-user/signup-user.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignupUserComponent,
+    UserDetailsComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
